e2f8b668326e3d89065d5744e0347f7e44472432
Laureano1997 <64487815+Laureano1997@users.noreply.github.com>
2025-05-03 03:12:26 -0300

    Update alarm.h

--- modules/alarm/alarm.h
+++ modules/alarm/alarm.h
@@ -1,1 +1,24 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _ALARM_H_
+#define _ALARM_H_
+
+//=====[Declaration of public defines]=========================================
+
+#define ALARM_BUZZ_TIME 500     //Mitad del período de el buzzer
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+void alarmInit();
+bool alarmLEDStateRead();
+bool alarmBuzzerStateRead();
+void alarmLEDStateWrite( bool state );
+void alarmBuzzerStateWrite( bool state );
+void alarmUpdate();
+
+//=====[#include guards - end]=================================================
+
+#endif // _ALARM_H_
 
