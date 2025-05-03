8737a4c1fb0878a5dc7095082ee4136b50e1f9a7
Laureano1997 <64487815+Laureano1997@users.noreply.github.com>
2025-05-03 03:26:41 -0300

    Create water_sensor.h

---
+++ modules/water_sensor/water_sensor.h
@@ -0,0 +1,18 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _WATER_SENSOR_H_
+#define _WATER_SENSOR_H_
+
+//=====[Declaration of public defines]=========================================
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+void waterSensorInit();
+void waterSensorUpdate();
+bool waterSensorRead();
+
+//=====[#include guards - end]=================================================
+
+#endif // _WATER_SENSOR_H_
