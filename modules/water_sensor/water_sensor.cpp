50e446ef93bbe051ea327870320816e66f093245
Laureano1997 <64487815+Laureano1997@users.noreply.github.com>
2025-05-03 03:27:10 -0300

    Add files via upload

---
+++ modules/water_sensor/water_sensor.cpp
@@ -0,0 +1,39 @@
+//=====[Libraries]=============================================================
+
+#include "mbed.h"
+
+#include "water_sensor.h"
+
+//=====[Declaration of private defines]========================================
+
+//=====[Declaration of private data types]=====================================
+
+//=====[Declaration and initialization of public global objects]===============
+
+DigitalIn  tankWaterSensor(D5);    //Sensor de agua en tanque
+
+//=====[Declaration of external public global variables]=======================
+
+//=====[Declaration and initialization of public global variables]=============
+
+//=====[Declaration and initialization of private global variables]============
+
+//=====[Declarations (prototypes) of private functions]========================
+
+//=====[Implementations of public functions]===================================
+
+void waterSensorInit()
+{
+    tankWaterSensor.mode(PullDown);
+}
+
+void waterSensorUpdate()
+{
+}
+
+bool waterSensorRead()
+{
+    return tankWaterSensor;
+}
+
+//=====[Implementations of private functions]==================================
